1a73040a9137404e769425cfe937fa6399e5a019
Nancy Boktor <nancyboktor1@gmail.com>
2021-10-27 21:41:55 -0400

    Adding The InterviewerListItem && InterviewerList Components && Style for them and testing them by storybook.

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const interviewersList = props.interviewers.map((interviewer) => (
+    <InterviewerListItem
+      key={interviewer.id}
+      avatar={interviewer.avatar}
+      name={interviewer.name}
+      selected={props.interviewer === interviewer.id}
+      setInterviewer={() => props.setInterviewer(interviewer.id)}
+    />
+  ));
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewersList}</ul>
+    </section>
+  );
+}
